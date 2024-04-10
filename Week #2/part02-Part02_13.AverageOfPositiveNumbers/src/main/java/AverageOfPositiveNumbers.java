0342977a11b571feb13ffd5073eff130480f6c07
Yevhen Kuzmenko <yhkuzmenko@gmail.com>
2024-04-10 09:54:09 +0300

    Added part02-Part02_13.AverageOfPositiveNumbers

---
+++ Week #2/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+            if (number < 0) {
+                continue;
+            }
+            count = count + 1;
+            sum = sum + number;
+        }
+        if (count == 0 || sum == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((double) sum / count);
+        }
+    }
+}
