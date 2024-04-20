f68db4fb7f92614eddc418accbb4e59787c3c302
Yevhen Kuzmenko <yhkuzmenko@gmail.com>
2024-04-20 15:47:20 +0300

    Added part03-Part03_28.FirstWords

---
+++ Week #3/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class FirstWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.equals("")) {
+                break;
+            }
+
+            String[] words = text.split(" ");
+            System.out.println(words[0]);
+        }
+    }
+}
